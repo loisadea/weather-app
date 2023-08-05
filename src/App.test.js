7ddc24356ef68401fae0e7b6c4482160d4b19685
Lois Adea <loisadea@gmail.com>
2023-08-05 23:16:55 +0100

    Add font, eslint rules, prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const titleElement = screen.getByText(/weather app/i);
   expect(titleElement).toBeInTheDocument();
